f437a3d21896d229c74cd20c39d84d26be12373f
meteoroh <adalove369@gmail.com>
2025-12-05 11:25:50 +0900

    port

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ def make_shell_context():
     return {'db': db, 'Performer': Performer, 'Video': Video, 'Settings': Settings}
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
